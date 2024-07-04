bcc3b0e73773e86d59980805ecebbec5fc1c8add
Anurag Gupta <anurag6967@gmail.com>
2024-07-04 13:13:59 +0530

    write userSchema in NextJs backend and yes it is different from NodeJs

---
+++ src/models/userModel.js
@@ -0,0 +1,40 @@
+import mongoose,{mongo, Schema} from "mongoose";
+import { type } from "os";
+
+const userSchema = new Schema(
+    {
+        username:{
+            type: String,
+            required: [true,"please enter your username"],
+            unique: true
+        },
+        email:{
+            type: String,
+            required: [true,"please enter your email"],
+            unique: true
+        },
+        password:{
+            type: String,
+            required: [true,"please enter your password"]
+        },
+        isVerified:{
+            type: Boolean,
+            default: false
+        },
+        isAdmin:{
+            type: Boolean,
+            default: false
+        },
+        verifyToken: String,
+
+        verifyTokenExpiry: Date,
+
+        forgattenPassword: String,
+
+        forgattenPasswordExpiry: Date,
+    }
+)
+
+const User =mongoose.models.users || mongoose.model("users", userSchema)
+
+export default User;
